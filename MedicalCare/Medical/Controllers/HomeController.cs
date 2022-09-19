615b28e0b287a7e5c86d3c1846f230dad509fced
unknown <khanhld0211@gmail.com>
2022-09-19 11:59:25 +0700

    new

---
+++ MedicalCare/Medical/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Medical.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //Action nhiệm vụ xử lý yêu cầu => trả về kết quả cho người dùng
+        //Kết quả có nhiều kiểu: VIew html, data json xml ,file
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+    }
+}
